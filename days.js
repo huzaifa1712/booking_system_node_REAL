4c6eb72f890ea190b8eaeb8496e135c684943722
Jeff Raghav <windowpane1712@gmail.com>
2017-09-08 08:47:22 +0800

    added a days Enum, created settings file

---
+++ days.js
@@ -0,0 +1,8 @@
+//Enum that defines days
+module.exports = days = {
+  MONDAY:"Monday",
+  TUESDAY:"Tuesday",
+  WEDNESDAY:"Wednesday",
+  THURSDAY:"Thursday",
+  FRIDAY:"Friday"
+};
---
+++ setting.js
@@ -0,0 +1,7 @@
+var days = require('./days.js');
+
+var settings = {
+  "times":["12:45,1:15,1:45"],
+  "days":[days.MONDAY,days.TUESDAY,days.WEDNESDAY,days.THURSDAY,days.FRIDAY],
+  "weeksAhead": 4, 
+}
