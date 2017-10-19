6107d709bc3852d59513dae046279a23de82b64e
Jeff Raghav <windowpane1712@gmail.com>
2017-10-19 16:25:43 +0800

    added a virtual property to the Settings Model

--- models/settingsModel.js
+++ models/settingsModel.js
@@ -13,8 +13,26 @@ var settingsSchema = mongoose.Schema({
 
 });
 
+//a virtual is a property that does not get persisted to the DB. Can call this property in code
+//have set this prop to the result of the returnTimes function so we can have
+//one property for the times and another for the thing to display in the table
+settingsSchema.virtual('returnTimes').get(function(){
+  var returnArray = [];
+  for(var i = 0; i < this.times.length-1; i++){
+    returnArray.push(this.times[i] + " - " + this.times[i+1]);
+  }
 
-module.exports = mongoose.model('Settings', settingsSchema);
+  return returnArray;
+});
+
+var Settings = mongoose.model('Settings', settingsSchema);
+var s = new Settings({
+  times:["12:45","1:15","1:45","2:15","2:45"],
+  days:[days.MONDAY,days.TUESDAY,days.WEDNESDAY,days.THURSDAY,days.FRIDAY],
+  weeksAhead:4
+});
+
+console.log(s.returnTimes[0]);
 
 //console.log(settings.days);
 
