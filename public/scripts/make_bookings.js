ac35777e8a86d4b10722e08537c503b951e6fc77
Jeff Raghav <windowpane1712@gmail.com>
2017-10-22 14:05:57 +0800

    FINALLY got pagination to work
    Seperated the submitBtn from the thing that loops through the tds and assigns the click events. That was causing the double booking
    
    Seperated by making it so that when you click on the td it stores its rowIndex and colIndex in the modal dialog. Later when submitBtn clicked, it takes that data, passes it into a function that returns the appropriate booking fields needed (e.g startDate and what not).
    
    Also the hover effect was still happening for the tds with the time, so fixed that by giving them a class .time and excluding that from the hover the same way .booked is.

--- public/scripts/make_bookings.js
+++ public/scripts/make_bookings.js
@@ -1,88 +1,75 @@
 
-$(document).ready(function(){
-  console.log("Make bookings.js")
-  console.log($("#bookings-table").data("weekNumber"))
-  console.log($("#bookings-table").data("year"));
-  //$(this).data('attr') == 'filled') - to identify elements with bookings
-  $('td').each(function(){
+
+function getBookingFields(rowIndex,columnIndex){
+  var tableCell = $('#bookings-table tr').eq(rowIndex + 1).find('td').eq(columnIndex);
+
+  var day =  $(tableCell).closest('table').find('th').eq(columnIndex).text().replace(/\s+/g, '');
+  console.log("Day: " + day);
+  var time =  $(tableCell).parent().find("td").first().text().replace(/\s+/g, '');
+  console.log("Time: " + time);
+
+  var times = time.split("-");
+  console.log("Times array: " + times);
+  var isoWeek = $("#bookings-table").data("weekNumber");
+  console.log("Week num: " + isoWeek);
+  console.log("is weeknum an int: " + Number.isInteger(isoWeek));
+
+  var year = $("#bookings-table").data("year");
+  console.log("Year: " + year);
+  console.log("is year an int: " + Number.isInteger(year));
+  console.log('startTime: ' + Time.startAndEndTimes(day,times,isoWeek,year).startDate);
+  console.log('endTime: ' + Time.startAndEndTimes(day,times,isoWeek,year).endDate);
+
+  //var name = $(this).text().replace(/\s+/g, '');
+  console.log("Date string to format: " + day + "-" + times[0] + "-" + isoWeek + "-" + year);
+  var startTime = Time.startAndEndTimes(day,times,isoWeek,year).startDate;
+  var endTime = Time.startAndEndTimes(day,times,isoWeek,year).endDate;
+  console.log("Created time variables");
+  console.log(startTime);
+  console.log(endTime);
+
+  return {
+    time:time,
+    startTime:startTime,
+    endTime:endTime
+  }
+}
+
+function makeBookings(){
+  $("#bookings-table td:not(:first-child)").each(function(){
     if($(this).hasClass("booked") == false){
       //add a click event if it is NOT filled
+
+      //Modal trigger
+     //sets it so that it opens a modal when clicked
+
+
+
       $(this).click(function(){
-        //sends booking through AJAX
-
-        //Modal trigger
         $(this).attr("data-toggle","modal");
-        $(this).attr("data-target","#booking-modal");  //sets it so that it opens a modal when clicked
+        $(this).attr("data-target","#booking-modal");
+        var rowIndex = $(this).closest("tr").index(); //starts from 0
+        var columnIndex = $(this).index(); //starts from 1 goes up to number of columns
 
-        var day =  $(this).closest('table').find('th').eq(this.cellIndex).text().replace(/\s+/g, '');
-        console.log("Day: " + day);
-        var time =  $(this).parent().find("td").first().text().replace(/\s+/g, '');
-        console.log("Time: " + time);
+        $("#booking-modal").data("rowIndex", rowIndex);
+        $("#booking-modal").data("colIndex", columnIndex);
 
-        var times = time.split("-");
-        console.log("Times array: " + times);
-        var isoWeek = $("#bookings-table").data("weekNumber");
-        console.log("Week num: " + isoWeek);
-        console.log("is weeknum an int: " + Number.isInteger(isoWeek));
+        //to select a td given the rowIndex and columnIndex:
+        //$('#bookings-table tr').eq(rowIndex + 1).find('td').eq(columnIndex).css('background-color','red');
+        console.log("Row Index: " + rowIndex);
+        console.log("Column Index: " + columnIndex);
 
-        var year = $("#bookings-table").data("year");
-        console.log("Year: " + year);
-        console.log("is year an int: " + Number.isInteger(year));
-        console.log('startTime: ' + Time.startAndEndTimes(day,times,isoWeek,year).startDate);
-        console.log('endTime: ' + Time.startAndEndTimes(day,times,isoWeek,year).endDate);
+        //var day =  $(this).closest('table').find('th').eq(this.cellIndex).text().replace(/\s+/g, '');
+        //console.log("Dayetc " +  day);
+      //  var bookingFields = getBookingFields(rowIndex,columnIndex);
+        //console.log(bookingFields);
 
-        //var name = $(this).text().replace(/\s+/g, '');
-        console.log("Date string to format: " + day + "-" + times[0] + "-" + isoWeek + "-" + year);
-        var startTime = Time.startAndEndTimes(day,times,isoWeek,year).startDate;
-        var endTime = Time.startAndEndTimes(day,times,isoWeek,year).endDate;
-        console.log("Created time variables");
-        console.log(startTime);
-        console.log(endTime);
 
+      });
+
+        //sends booking through AJAX
         //only submit and make booking if Submit button is clicked
-        $("#submitBtn").click(function(){
-          //if none of the buttons in the group are checked, alert to pick one
-          if(!$("input[name='reminder']:checked").val()){
-            alert("Please pick one of the reminder options!");
-          }
 
-          else{
-            var reminderValue = $("input[name='reminder']:checked").val();
-            console.log(reminderValue);
-            $.ajax({
-              type:'GET',
-              url:'/get_user',         //getting the user object so we can make a booking
-              success:function(response){
-                //response object is the user object
-                //Use moment to construct the date.
-                console.log("user: ");
-                console.log(response);
-                $.ajax({
-                  type: 'POST',
-                  url:'/make_booking',
-                  data: {
-                    id:response._id,
-                    name:response.name,
-                    email:response.email,
-                    timeString:time,
-                    startTime: startTime,
-                    endTime: endTime,
-                    reminder:reminderValue
-
-                  },
-                  success:function(response){
-                    //do nothing
-                    alert("Booking saved!");
-
-                    console.log(response);
-                  }
-                });
-                alert("Booking saved!");
-                window.location.reload();
-              }
-            });
-          }
-        });
 
 /*
         $.ajax({
@@ -99,9 +86,88 @@ $(document).ready(function(){
           }
         })*/
 
+
+    }
+  });
+
+}
+
+$(document).ready(function(){
+  console.log("Make bookings.js")
+  console.log($("#bookings-table").data("weekNumber"))
+  console.log($("#bookings-table").data("year"));
+  //$(this).data('attr') == 'filled') - to identify elements with bookings
+
+  $("#next-week").click(function(){
+    makeBookings();
+
+      //populateBookings(pageWeekNum);
+      //window.location.reload();
+  });
+
+  $("#prev-week").click(function(){
+    makeBookings();
+
+      //populateBookings(pageWeekNum);
+      //window.location.reload();
+  });
+
+  $("#submitBtn").click(function(){
+    //if none of the buttons in the group are checked, alert to pick one
+
+    //get the booking fields needed
+    //var bookingFields = getBookingFields();
+    var rowIndex = $("#booking-modal").data("rowIndex");
+    console.log(rowIndex);
+    var colIndex = $("#booking-modal").data("colIndex");
+
+    var bookingFields = getBookingFields(rowIndex,colIndex);
+
+    if(!$("input[name='reminder']:checked").val()){
+      alert("Please pick one of the reminder options!");
+    }
+
+    else{
+      var reminderValue = $("input[name='reminder']:checked").val();
+      console.log(reminderValue);
+
+      $.ajax({
+        type:'GET',
+        url:'/get_user',         //getting the user object so we can make a booking
+        success:function(response){
+          //response object is the user object
+          //Use moment to construct the date.
+          console.log("user: ");
+          console.log(response);
+          $.ajax({
+            type: 'POST',
+            url:'/make_booking',
+            data: {
+              id:response._id,
+              name:response.name,
+              email:response.email,
+              timeString:bookingFields.time,
+              startTime: bookingFields.startTime,
+              endTime: bookingFields.endTime,
+              reminder:reminderValue
+
+            },
+            success:function(response){
+              //do nothing
+              alert("Booking saved!");
+
+              console.log(response);
+            }
+          });
+          alert("Booking saved!");
+          window.location.reload();
+        }
       });
     }
   });
+
+
+  makeBookings();
 
 
 });
--- public/scripts/populate_bookings.js
+++ public/scripts/populate_bookings.js
@@ -44,6 +44,106 @@ function remove2AllBookings(){
     }
   });
 }
+function makeBookings(){
+  $('td').each(function(){
+    if($(this).hasClass("booked") == false){
+      //add a click event if it is NOT filled
+      $(this).click(function(){
+        //sends booking through AJAX
+
+        //Modal trigger
+        $(this).attr("data-toggle","modal");
+        $(this).attr("data-target","#booking-modal");  //sets it so that it opens a modal when clicked
+
+        var day =  $(this).closest('table').find('th').eq(this.cellIndex).text().replace(/\s+/g, '');
+        console.log("Day: " + day);
+        var time =  $(this).parent().find("td").first().text().replace(/\s+/g, '');
+        console.log("Time: " + time);
+
+        var times = time.split("-");
+        console.log("Times array: " + times);
+        var isoWeek = $("#bookings-table").data("weekNumber");
+        console.log("Week num: " + isoWeek);
+        console.log("is weeknum an int: " + Number.isInteger(isoWeek));
+
+        var year = $("#bookings-table").data("year");
+        console.log("Year: " + year);
+        console.log("is year an int: " + Number.isInteger(year));
+        console.log('startTime: ' + Time.startAndEndTimes(day,times,isoWeek,year).startDate);
+        console.log('endTime: ' + Time.startAndEndTimes(day,times,isoWeek,year).endDate);
+
+        //var name = $(this).text().replace(/\s+/g, '');
+        console.log("Date string to format: " + day + "-" + times[0] + "-" + isoWeek + "-" + year);
+        var startTime = Time.startAndEndTimes(day,times,isoWeek,year).startDate;
+        var endTime = Time.startAndEndTimes(day,times,isoWeek,year).endDate;
+        console.log("Created time variables");
+        console.log(startTime);
+        console.log(endTime);
+
+        //only submit and make booking if Submit button is clicked
+        $("#submitBtn").click(function(){
+          //if none of the buttons in the group are checked, alert to pick one
+          if(!$("input[name='reminder']:checked").val()){
+            alert("Please pick one of the reminder options!");
+          }
+
+          else{
+            var reminderValue = $("input[name='reminder']:checked").val();
+            console.log(reminderValue);
+            $.ajax({
+              type:'GET',
+              url:'/get_user',         //getting the user object so we can make a booking
+              success:function(response){
+                //response object is the user object
+                //Use moment to construct the date.
+                console.log("user: ");
+                console.log(response);
+                $.ajax({
+                  type: 'POST',
+                  url:'/make_booking',
+                  data: {
+                    id:response._id,
+                    name:response.name,
+                    email:response.email,
+                    timeString:time,
+                    startTime: startTime,
+                    endTime: endTime,
+                    reminder:reminderValue
+
+                  },
+                  success:function(response){
+                    //do nothing
+                    alert("Booking saved!");
+
+                    console.log(response);
+                  }
+                });
+                alert("Booking saved!");
+                window.location.reload();
+              }
+            });
+          }
+        });
+
+/*
+        $.ajax({
+          type:'POST',
+          url:'/make_booking',
+          data: {
+            day: day,
+            time:time,
+            name:name
+          },
+
+          success:function(response){
+            //do nothing
+          }
+        })*/
+
+      });
+    }
+  });
+}
 
 function clickBooking(){
   $(this).attr("data-toggle","modal");
@@ -119,6 +219,20 @@ function clickBooking(){
     }
   });
 }
+function removeAllBookings(){
+  console.log("Columns: " +$("table > tbody > tr:first > td").length);
+  var noOfColumns = $("table > tbody > tr:first > td").length;
+
+  $("#bookings-table td:not(:first-child)").each(function(){
+    if($(this).hasClass('booked')){
+      $(this).removeClass('booked');
+      //makeBookings();
+      //console.log(removed);
+    }
+    $(this).html(' ');
+  });
+
+  };
 function populateBookings(isoWeekNum){
   console.log("isoWeek from populate: " + isoWeekNum);
   $.ajax({
@@ -179,19 +293,7 @@ function populateBookings(isoWeekNum){
     }
   });
 }
-function removeAllBookings(){
-  console.log("Columns: " +$("table > tbody > tr:first > td").length);
-  var noOfColumns = $("table > tbody > tr:first > td").length;
 
-  for(var i = 2; i <= noOfColumns; i++ ){
-    if($('#bookings-table td:nth-child(' + i + ')').hasClass('booked')){
-      $('#bookings-table td:nth-child(' + i + ')').removeClass('booked');
-      //console.log(removed);
-    }
-    $('#bookings-table td:nth-child(' + i + ')').html(' ');
-  }
-
-  };
 
 
 
