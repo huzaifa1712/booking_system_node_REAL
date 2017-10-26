385ca0fa7b662dab407c6efeb656c7768aa373cc
Jeff Raghav <windowpane1712@gmail.com>
2017-10-26 18:28:47 +0800

    Can open modal populated with spaceName in admin settings

--- public/scripts/admin_settings.js
+++ public/scripts/admin_settings.js
@@ -13,6 +13,35 @@ function getSpaces(){
   return spaces;
 }
 
+//populates the spaces table with names of spaces, and assigns the space ID as data.
+function populateSpacesTable(){
+  var tBody = $('tbody');
+  var spaces = getSpaces();
+
+  for(var i = 0; i < spaces.length; i++){
+    var tr = $("<tr>");
+
+    var tdSpace = $("<td>" + spaces[i].name + "</td>");
+    tdSpace.data("spaceId", spaces[i]._id);
+    tdSpace.appendTo(tr);
+
+    tr.appendTo(tBody);
+  }
+}
+
+//goes through the table cells and assigns them a click event that opens the modal.
+function openModal(){
+  $("#spaces-table td").each(function(){
+    $(this).attr("data-toggle","modal");
+    $(this).attr("data-target","#edit-space-modal");
+
+    $(this).click(function(){
+      $("#spaceName").text($(this).text());
+    });
+  });
+}
+
 $(document).ready(function(){
-  
+  populateSpacesTable();
+  openModal();
 });
