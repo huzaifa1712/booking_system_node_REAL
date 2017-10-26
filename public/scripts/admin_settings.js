820094a57913b3b6daa3ab67d02fa55d3a294f71
Jeff Raghav <windowpane1712@gmail.com>
2017-10-27 01:46:17 +0800

    Can now view settings for the spaces in admin. Almost done!!!

--- public/scripts/admin_settings.js
+++ public/scripts/admin_settings.js
@@ -52,12 +52,53 @@ function populateModal(id){
   var times = space.times.map(function(time){
     return moment(time,"hh:mma").format("kk:mm");
   });
+  //days array
+  var days = space.days;
+
+  var daysDiv = $("#days");
+
+  //unchecks all checkboxes before checking those necessary.
+  $("#days input").each(function(){
+    $(this).prop("checked",false);
+  });
+
+  //goes through the array of days, then inside is another loop that loops through
+  //the checkboxes. If the day of the id corresponding to the box(#monday gives 'monday')
+  //is equal to the day, check the box.
+  days.forEach(function(day){
+    var idText = day.toLowerCase();
+    console.log("idText: " + idText);
+
+    $("#days input").each(function(){
+      var inputId = $(this).attr('id');
+      console.log("Input id:" + inputId);
+
+      if(inputId == idText){
+        $(this).prop("checked",true);
+      }
+    });
+  });
 
   var modalBody = $("#modal-body");
 
+  //removes all the time-group elements before populating with new ones.
+  $(".time-group").each(function(){
+    $(this).remove();
+  });
+
+  //populates the time elements. e.g 12:45 to 1:45, which is why the for loop
+  //goes from 0 to length-1, and uses i and i+1th elements of the array.
+  for(var i = 0; i < times.length - 1; i++){
+    var div = $('<div class = "row time-group">');
+    $('<input type = "time" value = "' + times[i] + '">').appendTo(div);
+    $('<span class = "middle"> to </span>').appendTo(div);
+    $('<input type = "time" value = "' + times[i + 1] + '">').appendTo(div);
+    div.appendTo(modalBody);
+  }
+  /*
   for(var i = 0; i < times.length; i++){
     $('<input type = "time" value = "' + times[i] + '">').appendTo(modalBody);
-  }
+  }*/
 }
 //goes through the table cells and assigns them a click event that opens the modal.
 function openModal(){
@@ -71,7 +112,7 @@ function openModal(){
       var spaceId = $(this).data("spaceId");
 
       $("#edit-space-modal").data("spaceId", spaceId);
-      //populateModal(spaceId);
+      populateModal(spaceId);
     });
   });
 }
