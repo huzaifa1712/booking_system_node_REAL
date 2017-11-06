31bed404b1eb190ebb4db442935cf80c0773ae5d
Jeff Raghav <windowpane1712@gmail.com>
2017-11-06 21:34:13 +0800

    created errorCodes function

--- excel.js
+++ excel.js
@@ -6,14 +6,18 @@ var moment = require('moment');
 
 var reminderArray = ["none", "4h","12h","1d","1w"];
 
+var workbook = XLSX.readFile(path.join(__dirname,'uploads/excel.xlsx'));
+var bookings = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
+var sheet_name_list = workbook.SheetNames;
+
 //https://stackoverflow.com/questions/37733966/get-the-given-date-format-the-string-specifying-the-format-in-javascript-or-mo
 //Above link is for specifying multiple different possible time parser formats
 //https://stackoverflow.com/questions/30859901/parse-xlsx-with-node-and-create-json
 
-var workbook = XLSX.readFile(path.join(__dirname,'uploads/excel.xlsx'));
-var sheet_name_list = workbook.SheetNames;
 
 //https://github.com/SheetJS/js-xlsx/issues/214 - For get_header_row
+
+//returns the headers without the unknowns.
 function getHeaders(workbook) {
     var headers = [];
     var sheet = workbook.Sheets.Sheet1
@@ -29,23 +33,13 @@ function getHeaders(workbook) {
 
         headers.push(hdr);
     }
+
+    //returns headers row without 'unknown'
+    headers = headers.filter(function(header){
+      return !header.includes('UNKNOWN');
+    });
     return headers;
 }
-
-//get the headers of the Excel file.
-var headers = getHeaders(workbook)
-
-//returns headers row without 'unknown'
-headers = headers.filter(function(header){
-  return !header.includes('UNKNOWN');
-});
-
-console.log(headers);
-//console.log(workbook);
-//this line of code gets an array where each element corresponds to one row of the
-//workbook
-var bookings = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-//console.log(bookings[0]);
 
 //generates the time fields, such as the time string and startTime and endTime date objects
 //Note: VARIABLE ACCESSIBILITY DEPENDS ON NAME OF HEADER IN THE ROW
@@ -71,12 +65,9 @@ function generateTimeFields(booking){
 
 }
 
-
-
 //Create a booking from the array received from excel
 //Note: VARIABLE ACCESSIBILITY DEPENDS ON NAME OF HEADER IN THE ROW
-
-function createBooking(booking){
+function createAndSaveBooking(booking){
   var timeFields = generateTimeFields(booking);
 
   var newBooking = new Booking();
@@ -101,11 +92,13 @@ function createBooking(booking){
 
 
 
-mongoose.connect('mongodb://localhost/bookings');
+//mongoose.connect('mongodb://localhost/bookings');
 
 //checks if there are any blank fields for the row for that booking.
 //Returns false if no missing properties.
 function missingProps(booking){
+  var headers = getHeaders(workbook);
+
   var missingProp = false;
   headers.forEach((header)=>{
     if(!booking.hasOwnProperty(header)){
@@ -125,47 +118,69 @@ function missingProps(booking){
 */
 function validateBookingFields(booking){
   var isErr = false;
-  var errorMsg = "ERROR:The following must be done:";
+  var errorCode = 0;
 
   //1.Each row must not have blanks.
   //check if the booking is missing props.
-  if(!missingProps(booking)){
-    errorMsg = errorMsg + "No fields for each booking are blank\n\n";
+  if(missingProps(booking)){
+    //errorMsg = errorMsg + "No fields for each booking are blank\n\n";
+    errorCode = 1;
     isErr = true;
   }
 
   else{
     //2. Email must end with @gapps.uwcsea.edu.sg
     if(!booking.email.endsWith('@gapps.uwcsea.edu.sg')){
-      errorMsg = errorMsg + "The e-mail for each booking is a UWCSEA email address.\n\n"
+      //errorMsg = errorMsg + "The e-mail for each booking is a UWCSEA email address.\n\n"
+      errorCode = 2;
       isErr = true;
     }
 
-    //3 and 4
+    //3 and 4 - doesn't really work
     try{
       var timeFields = generateTimeFields(booking);
     }
 
     catch(e){
       var isErr = true;
-      var errorMsg = errorMsg + "The date is in the format D-MMM-YY e.g 7-Nov-17, and times are 12 hour and hh:mma, e.g 1:45pm\n\n"
+      //var errorMsg = errorMsg + "The date is in the format D-MMM-YY e.g 7-Nov-17, and times are 12 hour and hh:mma, e.g 1:45pm\n\n"
+      errorCode = 3;
     }
 
     //5.Check reminder options
     var reminder = booking.reminder.toLowerCase().replace(/\s+/g, '');
     if(reminderArray.indexOf(reminder) == -1){
       isErr = true;
-      errorMsg = errorMsg + "The reminder option is either: " + reminderArray.join(',');
+      //errorMsg = errorMsg + "The reminder option is either of: " + reminderArray.join(',');
+      errorCode = 4;
     }
   }
+
+  return{
+    isErr:isErr,
+    errorCode:errorCode
+  }
 }
-bookings.forEach((booking)=>{
 
-});
+function returnErrorMsg(errorCodes){
+  var errorMsg = "ERROR: Ensure the following are done:\n\n"
+  var errorMessages = {
+    1:"No fields for each booking are blank\n\n",
+    2:"The e-mail for each booking is a UWCSEA email address.\n\n",
+    3:"The date is in the format D-MMM-YY e.g 7-Nov-17, and times are 12 hour and hh:mma, e.g 1:45pm\n\n",
+    4:"The reminder option is either of: " + reminderArray.join(',')
+  }
 
-var reminder = "0"
-var reminderArray = ["none", "4h","12h","1d","1w"];
-console.log(reminderArray.indexOf(reminder));
+  errorCodes.forEach((errorCode)=>{
+    errorMsg = errorMsg + errorMessages[errorCode];
+  });
+
+  return errorMsg;
+}
+
+
+
+
 
 
 /*
