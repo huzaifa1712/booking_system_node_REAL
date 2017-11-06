705e178338e618607bfa785addc46016f8760ebf
Jeff Raghav <windowpane1712@gmail.com>
2017-11-06 14:46:52 +0800

    Set out requirements for validateFields function

--- excel.js
+++ excel.js
@@ -4,21 +4,25 @@ var Booking = require('./models/booking');
 var mongoose = require('mongoose');
 var moment = require('moment');
 
+//https://stackoverflow.com/questions/37733966/get-the-given-date-format-the-string-specifying-the-format-in-javascript-or-mo
+//Above link is for specifying multiple different possible time parser formats
 //https://stackoverflow.com/questions/30859901/parse-xlsx-with-node-and-create-json
 
 var workbook = XLSX.readFile(path.join(__dirname,'uploads/excel.xlsx'));
 var sheet_name_list = workbook.SheetNames;
+//this line of code gets an array where each element corresponds to one row of the
+//workbook
 var bookings = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
 console.log(bookings[0]);
 
-function generateBookingFields(booking){
-  //replace spaces from Excel and construct time string e.g '12:45PM-1:15PM'
-  var time = booking.startTime.replace(/\s+/g, '') + '-' + booking.endTime.replace(/\s+/g, '');
+function generateTimeFields(booking){
+  //replace spaces from Excel and construct time string e.g '12:45pm-1:15pm' make times lowecase so PM/pm and am/AM doesn't matter
+  var time = booking.startTime.replace(/\s+/g, '').toLowerCase() + '-' + booking.endTime.replace(/\s+/g, '').toLowerCase();
   //get moment object from day String
 
-  //create timestrings to create moments from
-  var startTimeString = booking.day + "-" +  booking.startTime.replace(/\s+/g, '');
-  var endTimeString = booking.day + "-" +  booking.endTime.replace(/\s+/g, '');
+  //create timestrings to create moments from - make times lowecase so PM/pm and am/AM doesn't matter
+  var startTimeString = booking.day + "-" +  booking.startTime.replace(/\s+/g, '').toLowerCase();
+  var endTimeString = booking.day + "-" +  booking.endTime.replace(/\s+/g, '').toLowerCase();
 
   //create startTime and endTime Date objects
   var startTime = moment(startTimeString, "D-MMM-YY-hh:mma").toDate();
@@ -32,8 +36,18 @@ function generateBookingFields(booking){
 
 }
 
+/*
+1. Each row must be complete - no blanks in each space. if there is a row with complete blanks, skip it.
+2. Email must end with @gapps.uwcsea.edu.sg
+3. If invalid date, catch the error and tell the admin.
+4. Check that reminder option is one of the allowed. 'None' and blank are same.
+*/
+function validateBookingFields(booking){
+
+}
+
 function createBooking(booking){
-  var timeFields = generateBookingFields(booking);
+  var timeFields = generateTimeFields(booking);
 
   var newBooking = new Booking();
 
