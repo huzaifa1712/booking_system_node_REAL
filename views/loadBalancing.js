c334359f710c7db8761491d470fd71b8ecb58b45
Jeff Raghav <windowpane1712@gmail.com>
2017-11-07 09:23:10 +0800

    load balancing

--- views/loadBalancing.js
+++
@@ -1,8 +0,0 @@
-var Booking = require('./models/booking');
-var Space  = require('./models/space');
-
-/*
-  Search through bookings based on space name
-  Count the number of times each time appears.
-  Select the two
-*/
